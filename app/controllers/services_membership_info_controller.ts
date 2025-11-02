b6bf63d9ef63866560392e5f01c61d27c3570e6c
ayushman <chauhanayushman505@gmail.com>
2025-11-02 16:13:26 +0530

    membership controller is done

--- app/controllers/services_membership_info_controller.ts
+++ app/controllers/services_membership_info_controller.ts
@@ -1,38 +1,63 @@
+import ServiceMembershipInfos from '#models/service_membership_info'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class ServicesMembershipInfoController {
   /**
    * Display a list of resource
    */
-  async index({}: HttpContext) {}
+  async index({ response }: HttpContext) {
+    const servicesWithMembership = await ServiceMembershipInfos.query().preload('service')
+    return response.ok({ msg: 'all the services with memberships', data: servicesWithMembership })
+  }
+
 
   /**
    * Display form to create a new record
    */
-  async create({}: HttpContext) {}
+  async create({ }: HttpContext) { }
 
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    const data = request.all()
+    const membershipCreated = await ServiceMembershipInfos.create(data)
+    return response.ok({ msg: 'membership created successfully', data: membershipCreated })
+  }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params, response }: HttpContext) {
+    const membership = await ServiceMembershipInfos.query().where('id', params.id).preload('service')
+    if (!membership) return response.notFound({ msg: 'No membership exist' })
+    return response.ok({ msg: 'membership found', data: membership })
+  }
 
   /**
    * Edit individual record
    */
-  async edit({ params }: HttpContext) {}
+  async edit({ params }: HttpContext) { }
 
   /**
    * Handle form submission for the edit action
    */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request, response }: HttpContext) {
+    const payload = request.all()
+    const membership = await ServiceMembershipInfos.find(params.id)
+    if (!membership) return response.notFound({ msg: 'No membership exist' })
+    membership.merge(payload)
+    await membership.save()
+    return response.ok({ msg: 'updated successfully', membership })
+  }
 
   /**
    * Delete record
    */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, response }: HttpContext) {
+    const membership = await ServiceMembershipInfos.find(params.id)
+    if (!membership) return response.notFound({ msg: 'No membership exist' })
+    membership.delete()
+    return response.ok({ msg: 'deleted successfully' })
+  }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,6 @@
 
 import ServiceController from '#controllers/service_controller'
+import ServicesMembershipInfoController from '#controllers/services_membership_info_controller'
 import ServicesSlotInfoController from '#controllers/services_slot_info_controller'
 import UserController from '#controllers/user_controller'
 import router from '@adonisjs/core/services/router'
@@ -13,3 +14,4 @@ router.get('/', async () => {
 router.resource('users', UserController).apiOnly()
 router.resource('services', ServiceController).apiOnly()
 router.resource('serviceSlotInfo', ServicesSlotInfoController).apiOnly()
+router.resource('serviceMembershipInfo', ServicesMembershipInfoController).apiOnly()
